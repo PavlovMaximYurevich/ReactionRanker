c9d3391524525c3d7079a8a71352ccd581d40c86
PavlovMaxim <makc_im7@bk.ru>
2024-05-16 16:47:08 +0300

    реакции считаются корректно

--- orm_query.py
+++ orm_query.py
@@ -1,4 +1,4 @@
-from aiogram.types import Message, MessageReactionUpdated, ReactionCount
+from aiogram.types import Message, MessageReactionUpdated, ReactionCount, MessageReactionCountUpdated
 from sqlalchemy import update, select
 from sqlalchemy.ext.asyncio import AsyncSession
 
@@ -20,41 +20,40 @@ async def orm_create_or_update_reactions(session: AsyncSession,
                                          message_reaction: MessageReactionUpdated,
                                          message_id: int,
                                          ):
-    q = select(Reactions.reactions).where(Reactions.id_message == message_id)
+    sql = select(Reactions.count_reactions).where(Reactions.id_message == message_id)
 
-    result = await session.execute(q)
+    result = await session.execute(sql)
     # print('RESULT', result.fetchall())
 
     if result.fetchall():
         print("UPDATE")
         # print('RESULT', result.fetchall())
         # qqq = select(Reactions.reactions).where(Reactions.id_message == message_id)
-        qqq = select(Reactions.count_reactions).where(Reactions.id_message == message_id)
-        r = await session.execute(qqq)
-        list_reactions = r.fetchall()  # реакции из БД
+        sql_select = select(Reactions.count_reactions).where(Reactions.id_message == message_id)
+        reactions = await session.execute(sql_select)
+        list_reactions = reactions.fetchall()  # реакции из БД
         print('COUNT_REACTIONS', list_reactions)
-        count_list_msg = len(message_reaction.new_reaction)
+        flag = len(message_reaction.new_reaction) - len(message_reaction.old_reaction)
 
+        # flag = 1
+        # count_list_msg.pop()
+        # print('AFTER_POP', count_list_msg)
+        # if len(count_list_msg) == 1:
+        #     flag = 1
+        # else:
+        #     flag = -1
 
         query = update(Reactions).where(Reactions.id_message == message_id).values(
             id_message=message_reaction.message_id,
             reactions=str(message_reaction.new_reaction),
-            count_reactions=list_reactions[0][0]
+            count_reactions=list_reactions[0][0] + flag
         )
         await session.execute(query)
         await session.commit()
 
-        # print('FETCHALL', r.fetchall())
-        # print('ДЛИНА', len(r.fetchall()))
-        # print('Длина списка', reactions.count_reactions)
-        # else:
-        #     del message_reaction.new_reaction[-1]
-        #     print('UPDATE DEL', message_reaction.new_reaction)
     else:
         print('CREATE')
-        # for emoji in message_reaction.new_reaction:
-        # if emoji.emoji not in ['💩', '👎', '🤬', '😡']:
-        #         print('CREATE', message_reaction.new_reaction)
+
         session.add(Reactions(
             id_message=message_reaction.message_id,
             # count_reactions=len(message_reaction.new_reaction)
